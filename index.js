b7aabefabdee33115b964f91e3be9ff34a740a36
Horlie <50752056+Horlie@users.noreply.github.com>
2022-07-22 15:54:12 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-var randomNumber1 = Math.floor(Math.random() * 6)+1;
-document.querySelector("img.img1").setAttribute("src", "images/dice"+randomNumber1+".png");
-console.log(randomNumber1);
+function GenerateNumber(){
+    return Math.floor(Math.random() * 6)+1;
+}
 
-var randomNumber2 = Math.floor(Math.random() * 6)+1;
-document.querySelector("img.img2").setAttribute("src", "images/dice"+randomNumber2+".png");
-console.log(randomNumber2);
+document.querySelector("button").addEventListener("click", function(){
+    let randomNumber1 = GenerateNumber();
+    let randomNumber2 = GenerateNumber();
+    document.querySelector("img.img1").setAttribute("src", "images/dice"+randomNumber1+".png");
+    document.querySelector("img.img2").setAttribute("src", "images/dice"+randomNumber2+".png");
 
 if(randomNumber1 > randomNumber2){
-    document.querySelector("h1").innerHTML = "🚩 Player 1 wins!"
+    document.querySelector("h1").innerHTML = "🚩 Player 1 wins!";
 }
 else if(randomNumber1 < randomNumber2){
-    document.querySelector("h1").innerHTML = "Player 2 wins! 🚩"
+    document.querySelector("h1").innerHTML = "Player 2 wins! 🚩";
 }
 else if(randomNumber1 === randomNumber2){
-    document.querySelector("h1").innerHTML = "Draw"
+    document.querySelector("h1").innerHTML = "Draw";
 }
+
+document.querySelector("button").classList.add("pressed");
+setTimeout(function(){
+    document.querySelector("button").classList.remove("pressed")
+}, 100);
+});
